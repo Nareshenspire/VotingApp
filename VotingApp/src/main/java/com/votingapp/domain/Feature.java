48ad84a1cb435beaeab978a6c70771100fd19541
DESKTOP-P2L9SSK\pappi <pappireddynaresh361@gmail.com>
2024-05-04 08:15:55 +0530

    Adding the vote an comment join tables

---
+++ VotingApp/src/main/java/com/votingapp/domain/Feature.java
@@ -0,0 +1,43 @@
+package com.votingapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Feature {
+	
+	private int id;
+	private String title;
+	private String description;
+	private String status;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
